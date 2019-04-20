2d69afd95d0a42ee3d49b350ebcc5ad993245162
Jason Ross <jrossar@outlook.com>
2019-04-19 23:56:08 -0700

    Make ReadJsonFile a class, create RandomForest model in tf

--- ReadJsonFiles.py
+++ ReadJsonFiles.py
@@ -5,9 +5,39 @@ from labelme import utils
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from skimage import color
+from skimage import io
 
 from os import listdir
 from os.path import isfile, join
+
+class ReadFolder:
+    def woof(self):
+        print("sup")
+    def __init__(self, path):
+        self.path = path
+
+    def bark(self):
+        print("wassup")
+
+    def folder_files_to_list(self):
+        print(self.path)
+        files = [self.path + f for f in listdir(self.path) if isfile(join(self.path, f))]
+        x = []
+        y = []
+        for file in files:
+            with open(file) as json_file:
+                data = json.load(json_file)
+                x.append(utils.img_b64_to_arr(data['imageData']))
+                y.append(data['shapes'][0]['label'])
+        return x, y
+
+        def to_grayscale(pictures):
+            gray_pictures
+            for x in pictures:
+                gray_pictures.append(color.rgb2gray(x))
+            return gray_pictures
+'''
 mypath = 'C:\\Users\\jross\\OneDrive\\Data Analytics\\Machine Learning\\Project\\Project Data\\Labeled France Potholes\\'
 onlyfiles = [mypath + f for f in listdir(mypath) if isfile(join(mypath, f))]
 
@@ -37,23 +67,20 @@ for file in onlyfiles:
         data = json.load(json_file)
         x_training.append(utils.img_b64_to_arr(data['imageData']))
         y_training.append(data['shapes'][0]['label'])
+'''
+
 
 '''
 paramter: path to the folder containing json files
 works for labelme files
 returns: 2 lists, one with jpeg and one with labels
 '''
-def folder_files_to_list(path):
-    files = [mypath + f for f in listdir(mypath) if isfile(join(path, f))]
-    x = []
-    y = []
-    for file in files:
-        with open(file as json_file):
-            data = json.load(json_file)
-            x.append(utils.img_b64_to_arr(data['imageData']))
-            y.append(data['shapes'][0]['label'])
-    return x, y
 
+'''
+x_trainging1, y_training1 = folder_files_to_list('C:\\Users\\jross\\OneDrive\\Data Analytics\\Machine Learning\\Project\\Project Data\\Labeled France Potholes\\')
+print(x_trainging1[0])
+'''
+'''
 print(x_training[0])
 print(y_training[0])
 
@@ -65,20 +92,23 @@ from skimage import io
 
 print(x_training[0][:,:,0])
 x_training_gray = []
-
+'''
 '''
 accepts a list of pictures
 converts list into grayscale
 returns the list
 '''
+'''
 def to_grayscale(pictures):
     gray_pictures
     for x in pictures:
-    gray_pictures.append(color.rgb2gray(x))
+        gray_pictures.append(color.rgb2gray(x))
     return gray_pictures
-
+    '''
+'''
 #print(x_training_gray.shape[0])
 print(x_training_gray[0])
 #print(x_training_gray)
 io.imshow(x_training_gray[0])
 io.show()
+'''
